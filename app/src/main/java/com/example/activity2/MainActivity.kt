e5134ec8b183e25bebd3b03b09d5fadb68d61669
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-17 22:36:18 +0700

    Memberikan image yang bernama nila, disimpan dalam res bagain drawable. Dengan contentDescri "Foto Diri"

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -6,16 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -122,6 +125,13 @@ fun BasicCompose(modifier: Modifier = Modifier) {       // Memberikan parameter 
 
             ),
             modifier = Modifier.padding(top = 10.dp)    // .dp --> untuk lainnya
+        )
+
+        // Gambar foto diri
+        Image(
+            painter = painterResource(id = R.drawable.nila),
+            contentDescription = "Foto Diri",
+
         )
 
 
