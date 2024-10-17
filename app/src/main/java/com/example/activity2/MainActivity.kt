95fed1b728bc8fb276a610de240d363f5941d051
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-17 21:54:54 +0700

    Mengatur Layout compose dan aligment

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.activity2.ui.theme.Activity2Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +36,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicCompose(modifier: Modifier = Modifier) {       // Memberikan parameter --> Function defaultnya membutuhkan pengubah
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,         // Mengatur aligment
+
     )
+
+    {
+
+    }
+
+
 }
 
 @Preview(showBackground = true)
