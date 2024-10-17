bdaa01985d51eac1ceeddbf6a0c5ed1eb0b4d4ad
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-17 22:11:07 +0700

    Membuat string / kalimat halam login

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,6 +59,12 @@ fun BasicCompose(modifier: Modifier = Modifier) {       // Memberikan parameter 
 
             )
 
+        )
+
+        // Membuat Deskripsi "Ini adalah halaman Login"
+        Text(
+            text = "Ini adalah halaman Login",
+            
         )
 
 
