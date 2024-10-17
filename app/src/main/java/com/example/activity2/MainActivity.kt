027d92faa75b13f2d3945ee7525e3722ad48feb2
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-17 21:44:12 +0700

    Memanggil fungsi  BasicCompose dengan menerapkan padding dari inner padding

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -20,10 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Activity2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BasicCompose(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
